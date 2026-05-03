328db16905622098431efe432f7044a378f69bce
SAShimo <qlshuangzi@gmail.com>
2026-05-03 13:41:43 -0700

    4.1

--- AI_dictionary_reader/fake_pic.py
+++ AI_dictionary_reader/fake_pic.py
@@ -143,7 +143,31 @@ def generate_one_page(page_num):
         M = cv2.getRotationMatrix2D(center, angle, 1.0)
         final_img = cv2.warpAffine(final_img, M, (WIDTH, HEIGHT), borderValue=(255, 255, 255))
 
-    return final_img, page_labels 
+    angle = random.uniform(-1.5, 1.5) 
+    center = (WIDTH // 2, HEIGHT // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    
+    final_img = cv2.warpAffine(final_img, M, (WIDTH, HEIGHT), borderValue=(255, 255, 255))
+    
+    rotated_labels = []
+    for label in page_labels:
+        box = label["box"]
+        pts = np.array([
+            [box[0], box[1]], [box[2], box[1]], 
+            [box[2], box[3]], [box[0], box[3]]
+        ])
+        ones = np.ones(shape=(len(pts), 1))
+        pts_ones = np.concatenate([pts, ones], axis=1)
+        tr_pts = M.dot(pts_ones.T).T
+        
+        nx1, ny1 = tr_pts[:, 0].min(), tr_pts[:, 1].min()
+        nx2, ny2 = tr_pts[:, 0].max(), tr_pts[:, 1].max()
+
+        new_label = label.copy()
+        new_label["box"] = [float(nx1), float(ny1), float(nx2), float(ny2)]
+        rotated_labels.append(new_label)
+
+    return final_img, rotated_labels
 
 def default_converter(obj):
     if isinstance(obj, np.ndarray):
